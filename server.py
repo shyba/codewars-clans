c6de34887dc2558149b8675720b05e5f6c941741
Victor Shyba <victor.shyba@gmail.com>
2021-02-07 01:47:17 -0300

    initial version

---
+++ server.py
@@ -0,0 +1,45 @@
+import aiohttp
+import asyncio
+from fastapi import FastAPI
+from time import time
+
+app = FastAPI()
+app.last_refresh = 0
+app.scoreboard = []
+
+
+users = {
+        "shyba": 770,
+        "uncanned": 0,
+        "innng": 3,
+        "consoli": 2,
+        "luizdepra": 65,
+        "marcospb19": 12
+}
+refresh_every = 60
+
+
+async def fetch_state():
+    if (time() - app.last_refresh) > refresh_every:
+        app.last_refresh = time()
+        await asyncio.ensure_future(refresh_task())
+    return app.scoreboard
+
+async def refresh_task():
+    scoreboard = []
+    async with aiohttp.ClientSession() as session:
+        for user, initial_points in users.items():
+            url = f"https://www.codewars.com/api/v1/users/{user}"
+            async with session.get(url) as response:
+                response = await response.json()
+                print(response)
+                current_points = response["honor"]
+                scoreboard.append((current_points - initial_points, current_points, initial_points, user))
+    scoreboard.sort(reverse=True)
+    app.scoreboard = scoreboard
+
+
+@app.get("/")
+async def root():
+    state = await fetch_state()
+    return state
