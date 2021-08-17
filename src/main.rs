6cbe046548560d2cd8a75a262f58d39abe3939e9
Victor Shyba <victor.shyba@gmail.com>
2021-08-17 03:49:29 -0300

    warp skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::any().map(|| {"olá"});
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
