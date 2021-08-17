c196b1c3a818c3c43b71691eff6baec83ab8164e
Victor Shyba <victor.shyba@gmail.com>
2021-08-17 04:14:12 -0300

    render Tera

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 use warp::Filter;
+use tera::{Tera, Context};
+
+fn default() -> String {
+    let tera = match Tera::new("templates/**/*.html") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+    let mut context = Context::new();
+    tera.render("index.html", &context).unwrap()
+}
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::any().map(|| {"olá"});
+    let hello = warp::any().map(default);
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
