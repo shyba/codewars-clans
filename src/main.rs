0e5fba11e5a4f4e240e1f2dca6cb204d83c34ada
Victor Shyba <victor.shyba@gmail.com>
2021-08-17 05:20:06 -0300

    add hyper, serde, hyper-tls, serde_json and fetch score from API

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+use warp::Buf;
 use warp::Filter;
 use tera::{Tera, Context};
 use serde::Serialize;
+use hyper::{Client, Uri};
+use hyper_tls::HttpsConnector;
+use std::convert::Infallible;
+use serde_json::Value;
 
-#[derive(Debug, Serialize)]
+#[derive(Debug)]
+struct User {
+    name: String,
+    initial_points: i32,
+}
+
+#[derive(Debug, Serialize, Clone)]
 struct Score {
     user: String,
     points: i32,
@@ -10,7 +21,17 @@ struct Score {
     current_points: i32
 }
 
-fn default() -> String {
+async fn fetch_scores() -> Result<Vec<Score>, hyper::Error> {
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let res = client.get(Uri::from_static("https://www.codewars.com/api/v1/users/shyba")).await?;
+    let body = hyper::body::aggregate(res).await?;
+    let result: Value = serde_json::from_reader(body.reader()).unwrap();
+    println!("{:?}", result);
+    Ok(vec![])
+}
+
+async fn default() -> Result<String, Infallible> {
     let tera = match Tera::new("templates/**/*.html") {
         Ok(t) => t,
         Err(e) => {
@@ -19,13 +40,18 @@ fn default() -> String {
         }
     };
     let mut context = Context::new();
-    context.insert("scores", &[Score {user: "Victor".to_string(), points: 0, initial_points: 0, current_points: 0}]);
-    tera.render("index.html", &context).unwrap()
+    context.insert("scores", &fetch_scores().await.unwrap_or_else(|what| {
+        println!("{:?}", what);
+        [].to_vec()}));
+    Ok(tera.render("index.html", &context).unwrap())
 }
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::any().map(default);
+    let hello = warp::any().and_then(
+        || async {
+            default().await
+        });
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
